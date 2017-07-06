97c0da6cb1f9903b569b12f3024a32f87150237f
u222471 <lukas.elsner@sbb.ch>
2017-07-06 12:33:24 +0200

    migrate android related stuff

--- tools/android-sdk-update.sh
+++ tools/android-sdk-update.sh
@@ -2,7 +2,7 @@
 
 mkdir -p /opt/android-sdk-linux/tools/bin/
 cp /opt/tools/android-env.sh /opt/android-sdk-linux/tools/bin/
-source /opt/android-sdk-linux/tools/bin/androidenv.sh
+source /opt/android-sdk-linux/tools/bin/android-env.sh
 
 cd ${ANDROID_SDK_HOME}
 
