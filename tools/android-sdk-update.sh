f290af6b0436a4e050663a26c642bf88fff459f2
u222471 <lukas.elsner@sbb.ch>
2017-07-06 09:54:22 +0200

    migrate android related stuff

--- tools/android-sdk-update.sh
+++ tools/android-sdk-update.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-
-export ANDROID_SDK_HOME=/opt/android-sdk-linux
-export PATH=${PATH}:${ANDROID_SDK_HOME}/platform-tools:${ANDROID_SDK_HOME}/tools/bin:${ANDROID_SDK_HOME}/emulator:/opt/tools
-
+. /opt/android-sdk-linux/tools/bin/androidenv.sh
 
 cd ${ANDROID_SDK_HOME}
 
@@ -19,11 +16,14 @@ fi
 echo "Copying Licences"
 cp -r /opt/licenses ${ANDROID_SDK_HOME}/licenses
 
+echo "Copying Licences"
+cp -r /opt/tools/*.sh ${ANDROID_SDK_HOME}/tools/bin
+
 echo "Installing packages"
-/opt/tools/android-accept-licenses.sh "sdkmanager --package_file=/opt/tools/package-list.txt --verbose"
+android-accept-licenses.sh "sdkmanager --package_file=/opt/tools/package-list.txt --verbose"
 
 echo "Updating SDK"
-/opt/tools/android-accept-licenses.sh "sdkmanager --update --verbose"
+update_sdk
 
 echo "Accepting Licenses"
-/opt/tools/android-accept-licenses.sh "sdkmanager --licenses --verbose"
+android-accept-licenses.sh "sdkmanager --licenses --verbose"
