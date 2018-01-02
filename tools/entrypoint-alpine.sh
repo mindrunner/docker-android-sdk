37e447d7a617e15cef9feec7d20a1cb8acd4b287
Khoa <khoa.tan.nguyen.96@gmail.com>
2018-01-02 14:21:24 -0800

    entrypoint file uses su-exec for both images

--- tools/entrypoint-alpine.sh
+++
@@ -1,2 +0,0 @@
-chown android:android /opt/android-sdk-linux
-exec su-exec android:android /opt/tools/android-sdk-update.sh "$@"
--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -1,8 +1,2 @@
-#!/usr/bin/env bash
-
-su_mt_user() {
-    su android -c '"$0" "$@"' -- "$@"
-}
-
 chown android:android /opt/android-sdk-linux
-su_mt_user /opt/tools/android-sdk-update.sh ${1}
+exec su-exec android:android /opt/tools/android-sdk-update.sh "$@"
