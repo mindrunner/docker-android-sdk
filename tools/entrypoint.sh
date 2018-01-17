fc89499b6c3ea52710b9b5f358f8606d7fe828cc
lukas <open@mindrunner.de>
2018-01-17 14:26:56 +0100

    Update entrypoint.sh

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -1,2 +1,24 @@
+#!/bin/bash
+
+function checkbin() {
+    type -P su-exec
+}
+
+function su_mt_user() {
+    su android -c '"$0" "$@"' -- "$@"
+}
+
 chown android:android /opt/android-sdk-linux
-exec su-exec android:android /opt/tools/android-sdk-update.sh "$@"
+
+if checkbin; then
+    exec su-exec android:android /opt/tools/android-sdk-update.sh "$@"
+else
+    su_mt_user /opt/tools/android-sdk-update.sh ${1}
+fi
+
+
+
+
+
+
+
