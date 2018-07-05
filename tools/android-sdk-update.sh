c2340f1420fbbfe6e70250c4801800a83f36fc0a
lukas <open@mindrunner.de>
2018-07-05 13:26:02 +0200

    Update android-sdk-update.sh
    update SDK linux bootstrap zip

--- tools/android-sdk-update.sh
+++ tools/android-sdk-update.sh
@@ -27,7 +27,7 @@ then
     echo "SDK Tools already bootstrapped. Skipping initial setup"
 else
     echo "Bootstrapping SDK-Tools"
-    wget -q http://dl.google.com/android/repository/sdk-tools-linux-3859397.zip -O sdk-tools-linux.zip \
+    wget -q http://dl.google.com/android/repository/sdk-tools-linux-4333796.zip -O sdk-tools-linux.zip \
       && unzip sdk-tools-linux.zip \
       && touch .bootstrapped \
       && rm sdk-tools-linux.zip
