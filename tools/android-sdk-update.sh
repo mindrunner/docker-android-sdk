1f27188597a7ca5c65daa5984fa4d12c80650efa
lukas <open@mindrunner.de>
2018-11-09 17:54:05 +0100

    implement workaround for # https://stackoverflow.com/questions/35128229/error-no-toolchains-found-in-the-ndk-toolchains-folder-for-abi-with-prefix-llv

--- tools/android-sdk-update.sh
+++ tools/android-sdk-update.sh
@@ -65,3 +65,9 @@ update_sdk
 
 echo "Accepting Licenses"
 android-accept-licenses.sh "sdkmanager ${SDKMNGR_OPTS} --licenses"
+
+# https://stackoverflow.com/questions/35128229/error-no-toolchains-found-in-the-ndk-toolchains-folder-for-abi-with-prefix-llv
+( cd /opt/android-sdk-linux/ndk-bundle/toolchains \
+&& ln -s aarch64-linux-android-4.9 mips64el-linux-android \
+&& ln -s arm-linux-androideabi-4.9 mipsel-linux-android )
+
